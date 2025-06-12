c609c39ecffc87ace1a90d30a13d73c807e3681f
Hkr3d2 <151843832+Hkr3d2@users.noreply.github.com>
2025-06-12 08:57:54 +0200

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -4,8 +4,6 @@ sudo apt install xfce4 xfce4-terminal tightvncserver firefox gedit
 cd
 git clone https://github.com/novnc/noVNC
 pip install websockify
-echo startxfce4 >> ~/.vnc/xstartup
-echo firefox >> ~/.vnc/xstartup
 vncserver :1
 vncserver -kill :1
 cd
@@ -17,3 +15,5 @@ echo python3 -m websockify --web . 6080 localhost:5901 >> vnc.sh
 echo vncserver -kill :1 >> vnc.sh
 chmod +x vnc.sh
 echo /home/codespace/vnc.sh >> ~/.bashrc
+echo startxfce4 >> ~/.vnc/xstartup
+echo firefox >> ~/.vnc/xstartup
